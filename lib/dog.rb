50c6a6763fc11c9f948aa1bae1c571d08c5c5ca0
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-15 20:52:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
 class Dog 
+  
   Dog.new
-  def name=(x)
+  
+  def name(x)
     x = @dogsname 
   end
+  
   def name 
     @dogsname
   end
+  
   def bark 
     puts "woof!"
   end
+
 end
     
